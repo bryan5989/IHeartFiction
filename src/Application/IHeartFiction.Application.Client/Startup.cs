e85ef308ea102a3a61f8969ac3205ee5eb5a9205
Johnathan Irvin <IrvinJohnathan@gmail.com>
2019-07-23 02:08:11 -0400

    Bootstrap, and layout changes. Starting to work on frontend.

--- src/Application/IHeartFiction.Application.Client/Startup.cs
+++ src/Application/IHeartFiction.Application.Client/Startup.cs
@@ -1,3 +1,11 @@
+/* Maintained By Johnathan P. Irvin
+ *
+ * ======== Disclaimer ========
+ * This software may use 3rd party libraries, that have their own seperate licenses.
+ * This software uses the GNU, General Public License (GPL v3) as defined at the link below.
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
